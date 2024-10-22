2a30a9a5e1edeb449bcebab66febac0052997b30
MusuxMedia <soporte@musuxmedia.com>
2024-10-22 17:51:22 -0300

    El fake entry ahora devuelve response

--- app/fakeentry/Demedia.py
+++ app/fakeentry/Demedia.py
@@ -3,12 +3,15 @@ from fastapi import HTTPException
 import requests
 
 
+
+
+
 class Debmedia:
-    def fake_entry(self, consultorio: str, token: str, pantallas: list[int], server: str, paciente: str = "",
+    def fake_entry(self, consultorio: str, token: str, pantallas: list[int], server: str,
                    turno: str = ""):
         url = self.validar_url(server)
 
-        payload = self.__get_payload(screen_ids=pantallas, turno=turno, puesto=consultorio, paciente=paciente)
+        payload = self.__get_payload(screen_ids=pantallas, turno=turno, puesto=consultorio)
         headers = {
             'x-api-key': token,
             'Content-Type': 'application/json'
@@ -18,13 +21,13 @@ class Debmedia:
         except requests.RequestException:
             raise HTTPException(status_code=404, detail="Server Incorrecto")
         self.validar_response(response)
+        return self.llamadorResponse(turno=turno, consultorio=consultorio)
 
-    def __get_payload(self, screen_ids: list[int], turno: str, puesto: str, paciente: str):
+    def __get_payload(self, screen_ids: list[int], turno: str, puesto: str):
         return json.dumps({
             "screen.ids": screen_ids,
             "column2": puesto,
             "column0": turno,
-            "column1": paciente
         })
 
     def validar_response(self, response: requests.Response):
@@ -33,3 +36,6 @@ class Debmedia:
 
     def validar_url(self, server: str):
         return f"https://{server}.debmedia.com/api/screen/fakeentry"
+
+    def llamadorResponse(self, turno:str, consultorio:str):
+        return  {f"nombrePaciente":turno,"consultorio":consultorio,"resultado":"OK"}
--- app/main.py
+++ app/main.py
@@ -17,9 +17,10 @@ async def request(data: RequestModel, credenciales: Annotated[Settings, Depends(
     request = ProcessOrganizer(data.dni, credenciales)
     return request.validate_data()
 
-@app.get("/llamador", status_code=status.HTTP_204_NO_CONTENT)
-def fake_entry(consultorio: str, display: str, token: str, nombre: str = "", turno: str = "",
+@app.get("/llamador", status_code=status.HTTP_200_OK)
+def fake_entry(consultorio: str, display: str, token: str, turno: str,
                server: str = "debqclients") -> None:
     display = DisplayParser().parse(pantallas=display)
-    Debmedia().fake_entry(consultorio=consultorio, token=token, pantallas=display, paciente=nombre, turno=turno,
+    return Debmedia().fake_entry(consultorio=consultorio, token=token, pantallas=display, turno=turno,
                           server=server)
+
